8d5f4e0cbb1cdf8d6f952debc33941acb506bd71
takeira <takeirabridges@gamil.com>
2023-11-16 15:43:41 -0500

    push

--- SwiftUIStoryTemplate/Chapter1/pageONE.swift
+++ SwiftUIStoryTemplate/Chapter1/pageONE.swift
@@ -8,11 +8,63 @@
 import SwiftUI
 
 struct pageONE: View {
+    
+    @State private var isCarOnFire = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("nightsky")
+                .resizable()
+                .ignoresSafeArea()
+            
+            ZStack {
+                ScrollView {
+                    Spacer()
+                    VStack{
+                        Text("Chapter One")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                        Text("The Fire")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                        Text("Once upon a time, deep in the trenches of Detroit, there was a haunted yet beautiful car named Analine. For months it operated as a normal vehicle, but on Halloween night every year the evil that had been festering for months had the chance to be recognized. Until one night, a fire broke out next door, causing the paint on Analine to melt. Because of that Analine's one of a kind exterior had been burned and bruised with soot. The blistering heat from the fire allowed the true evil and wickedness that lied within the car, unbeknownst to Analine and it's owner, to awaken 5 days early. Though Analine was not aesthetically pleasing to look at, it still maintained all its functionallity and more.")
+                            .lineSpacing(5.5)
+                            .foregroundColor(.white)
+                            .padding()
+                        
+                        
+                    }
+                    
+                    
+                }
+                ZStack{
+                        Button {isCarOnFire.toggle()
+                        } label: {
+                            Image("redcar")
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                        } .offset(x:0, y:270)
+                    
+                        if isCarOnFire {
+                            VStack{
+                                Spacer()
+                                Image ("fire")
+                                    .resizable()
+                                    .scaledToFit()
+                            }
+                            .ignoresSafeArea()
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+
+struct pageOne_Previews: PreviewProvider {
+    static var previews: some View {
+        pageONE()
     }
 }
 
-#Preview {
-    pageONE()
-}
