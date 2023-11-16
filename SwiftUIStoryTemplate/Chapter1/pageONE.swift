36a69492649ce2925f7a6afc42e528c476097542
takeira <takeirabridges@gamil.com>
2023-11-16 15:38:34 -0500

    chapter one ui

---
+++ SwiftUIStoryTemplate/Chapter1/pageONE.swift
@@ -0,0 +1,18 @@
+//
+//  pageONE.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Takeira Bridges on 11/16/23.
+//
+
+import SwiftUI
+
+struct pageONE: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    pageONE()
+}
