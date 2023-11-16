cfa423eb05cf6acabec03fc70ddd2b62cbc9cbeb
Daniel <danielisiguzo93@gmail.com>
2023-11-16 16:16:26 -0500

    Added Images for story on page 1

--- SwiftUIStoryTemplate/Chapter2View.swift
+++ SwiftUIStoryTemplate/Chapter2View.swift
@@ -2,7 +2,26 @@ import SwiftUI
 
 struct Chapter2View: View {
     var body: some View {
-        Text("Chapter 2")
+        // Vstack -> Verticle
+        // Hstack -> Horizontal
+        // Zstack -> zIndex (back to front)
+        ZStack{
+            Image("joseph")
+                .resizable()
+                .scaledToFit()
+            Text("But the car wasn't always a vessel for evil. Just a few years ago, the car was a normal srt with a beautiful azure color. Its owner was a man named Joesph. Joseph named the car Adaline. Joseph was a kind hearted man, who lived in the westide of Detroit. But two blocks down from Joseph's house lived a man named Meechie. Meechie was a hater, he was mad that his car wasn't as good as Joseph's so he hired three men to aquire the car from Joseph.  Three theives were named.")
+               
+                .font(.title)
+                .foregroundStyle(.black)
+                .padding()
+                .background(
+                    Rectangle()
+                        .foregroundStyle(.gray)
+                        .opacity(0.5)
+                    
+                )
+            
+        }
     }
 }
 
