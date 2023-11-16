baa7d09b77ddf892b010abe60942558058dcbf43
takeira <takeirabridges@gamil.com>
2023-11-16 17:00:54 -0500

    My changes

--- SwiftUIStoryTemplate/Chapter1/pageTHREE.swift
+++ SwiftUIStoryTemplate/Chapter1/pageTHREE.swift
@@ -19,6 +19,7 @@ struct pageTHREE: View {
             Image("nightsky")
                 .resizable()
                 .ignoresSafeArea()
+            
             Text("With 5 days left before returning back to normal Analine's main goal was to invoke fear in those who displayed it least. The burned, dilapitated car would slowly trail behind those who chose to roam the streets at night. After capturing what seemed to be endless victims, a teenage boy was captured, then a cold chill ran through the boys body after reading greeting cards that the car had printed for previous victims. They read the following....Welcome to Paradise! You are in for a ride! You can expect Pain on this journey. So sit back, relax, and see what your future awaits.")
                 .lineSpacing(20)
                 .foregroundColor(.white)
@@ -26,34 +27,11 @@ struct pageTHREE: View {
             
             ZStack{
                 VStack{
-                    Button {printTwoShowing.toggle()
-                    } label: {
-                        Rectangle()
-                            .foregroundColor(.white)
-                            .frame(width:110, height:140)
-                            .offset(x: -100)
-                        .rotationEffect(.degrees(22))}
-                        
-                        Rectangle()
-                            .foregroundColor(.white)
-                            .frame(width:110, height:140)
-                            .offset(x: 100)
-                            .rotationEffect(.degrees(-22))
-                        Rectangle()
-                            .foregroundColor(.white)
-                            .frame(width:110, height:140)
-                            .offset(x: -100)
-                            .rotationEffect(.degrees(22))
-                        Rectangle()
-                            .foregroundColor(.white)
-                            .frame(width:110, height:140)
-                            .offset(x: 100)
-                            .rotationEffect(.degrees(-22))
-                        
+                       
+                        }
                     }
                 }
             }
-        }
     }
 #Preview {
     pageTHREE()
